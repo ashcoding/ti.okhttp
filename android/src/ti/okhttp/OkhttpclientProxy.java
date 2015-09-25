16e40de7243e647cbaca1fa0daaea98fc51285a0
Ashraf <msamah@appcelerator.com>
2015-09-25 11:56:44 +0800

    Updated java files with license

--- android/src/ti/okhttp/OkhttpclientProxy.java
+++ android/src/ti/okhttp/OkhttpclientProxy.java
@@ -1,3 +1,10 @@
+/**
+ * Appcelerator Titanium Mobile
+ * Copyright (c) 2015 by Appcelerator, Inc. All Rights Reserved.
+ * Licensed under the terms of the Apache Public License
+ * Please see the LICENSE included with this distribution for details.
+ * 
+ */
 package ti.okhttp;
 
 import java.io.IOException;
